4b5c681bb09b87a48c44297ae05b650a9f462ee2
aaaawt <weiting54@outlook.com>
2021-12-02 23:50:50 +0800

    update traindataset

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import torch
 from torchvision import datasets, transforms
+from tqdm import tqdm
 
 
 def main():
     train_dataset = datasets.ImageFolder('datasets/TinyImageNet/train',
                                          transform=transforms.Compose([transforms.ToTensor()]))
-    for i in range(len(train_dataset)):
+    for i in tqdm(range(len(train_dataset))):
         if train_dataset[i][0].size() != torch.Size([3, 64, 64]):
             print(i)
             break
     else:
-        print('hhhhh')
+        print('end')
 
 
 if __name__ == '__main__':
