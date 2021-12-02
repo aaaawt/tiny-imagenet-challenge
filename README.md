244f3c6396a1cf9c2ec631e54247b80aae6233ba
aaaawt <weiting54@outlook.com>
2021-12-02 23:32:34 +0800

    train

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ git clone git@github.com:aaaawt/tiny-imagenet-challenge.git
 cd tiny-imagenet-challenge
 cd datasets
 unzip TinyImageNet.zip
+cd ..
+pip3 install -r reqirements.txt
 ```
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import torch
+from torchvision import datasets, transforms
+
+
 def main():
-    print('hello, world!')
+    train_dataset = datasets.ImageFolder('datasets/TinyImageNet/train',
+                                         transform=transforms.Compose([transforms.ToTensor()]))
+    for i in range(len(train_dataset)):
+        if train_dataset[i][0].size() != torch.Size([3, 64, 64]):
+            print(i)
+            break
+    else:
+        print('hhhhh')
 
 
 if __name__ == '__main__':
